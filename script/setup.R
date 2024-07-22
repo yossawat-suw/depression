f430cc73737f5b4615362ed462da3be9fcae27b0
point0907 <point0907@gmail.com>
2024-07-22 09:05:31 +0700

    #add multicollinearity analysis

--- script/setup.R
+++ script/setup.R
@@ -12,4 +12,13 @@ install.packages("writexl")
 install.packages("labelled")
 install.packages("data.table")
 install.packages("stringr")
-install.packages("effects")
+#install.packages("effects") #to visualize the polr
+install.packages("car") 
+install.packages("glmnet") #for penalized regression
+
+install.packages("tidyverse")
+install.packages("rcompanion")
+install.packages("corrr") #for the asso node plot
+install.packages("corrplot") #for the asso matrix plot
+install.packages("caret")
+install.packages("collinear")
